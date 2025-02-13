58df9c37abd691e1d31d00b7407fa17fc031fc14
ekiciburak <ekcburak@hotmail.com>
2025-02-13 16:24:24 +0000

    some basics

--- type/lcontext.v
+++ type/lcontext.v
@@ -4,14 +4,34 @@ From LIMR Require Import src.expressions src.header type.local.
 Require Import List String Coq.Arith.PeanoNat Morphisms Relations.
 Import ListNotations. 
 
-Inductive tctx: Type :=
+Notation tctx := (list (part*ltt)) (only parsing).
+(* Inductive tctx: Type :=
   | tnil : tctx
   | tcons: part -> ltt -> tctx -> tctx.
+ *)
+Class ctx: Type :=
+  mkCt
+  { und: tctx;
+    reg: NoDup (map fst und)
+  }.
+
+(*shadows*)
+Fixpoint typeof (c: tctx) (p: part): option ltt :=
+  match c with
+    | nil         => None
+    | (q,t) :: c' => if eqb p q then Some t else typeof c' p
+  end. 
+
+Fixpoint dom (c: tctx): list part :=
+  match c with
+    | nil         => nil
+    | (p,t) :: c' => p :: dom c'
+  end.
 
 Fixpoint appc (c1 c2: tctx): tctx :=
   match c1 with
-    | tnil => c2
-    | tcons p t ts => tcons p t (appc ts c2)
+    | nil => c2
+    | (p,t) :: ts => (p,t) :: (appc ts c2)
   end.
 
 Inductive label: Type :=
@@ -23,19 +43,22 @@ Check onth.
 
 Inductive tctxR: tctx -> label -> tctx -> Prop :=
   | Rsend: forall p q xs n s T,
+           p <> q ->
            onth n xs = Some (s, T) ->
-           tctxR (tcons p (ltt_send q xs) tnil) (lsend p q (Some s)) (tcons p T tnil)
+           tctxR [(p,(ltt_send q xs))] (lsend p q (Some s)) [(p,T)]
   | Rrecv: forall p q xs n s T,
+           p <> q ->
            onth n xs = Some (s, T) ->
-           tctxR (tcons p (ltt_recv q xs) tnil) (lrecv p q (Some s)) (tcons p T tnil)
+           tctxR [(p,(ltt_recv q xs))] (lrecv p q (Some s)) [(p,T)]
   | Rcomm: forall p q g1 g1' g2 g2' s s', 
+           p <> q ->
            tctxR g1 (lsend p q (Some s)) g1'  ->
-           tctxR g2 (lsend p q (Some s')) g2' ->
+           tctxR g2 (lrecv q p (Some s')) g2' ->
            subsort s s' ->
            tctxR (appc g1 g2) (lcomm p q) (appc g1' g2')
   | Rvar : forall g l g' p T,
            tctxR g l g' ->
-           tctxR (tcons p T g) l (tcons p T g').
+           tctxR (appc g [(p,T)]) l (appc g' [(p,T)]).
 
 Definition tctxRE l c := exists c', tctxR c l c'.
 
@@ -91,14 +114,20 @@ Definition liveness_inner (pt: Path): Prop :=
 Definition live_gfp := alwaysC (liveness_inner).
 
 (* example *)
-CoFixpoint inf_path := cocons (tnil, (lcomm "p" "q")) inf_path.
+CoFixpoint inf_path := cocons ((@nil (string * ltt)), (lcomm "p" "q")) inf_path.
 
-Lemma nilDec: forall g1 g2, appc g1 g2 = tnil -> g1 = tnil /\ g2 = tnil.
+Lemma nilDec: forall g1 g2, appc g1 g2 = nil -> g1 = nil /\ g2 = nil.
 Proof. intro g1.
        induction g1; intros.
        - simpl in H. subst. easy.
-       - simpl in H.
-         easy.
+       - simpl in H. destruct a. easy.
+Qed.
+
+Lemma appcNnil: forall g p T, appc g [(p,T)] = nil -> False.
+Proof. intro g.
+       induction g; intros.
+       - simpl in H. easy.
+       - simpl in H. destruct a. easy.
 Qed.
 
 Theorem inf_fair: fair_gfp inf_path.
@@ -115,148 +144,63 @@ Proof. red.
        apply nilDec in H2.
        destruct H2 as (H2a, H2b).
        subst.
-       easy.
+       inversion H6. subst. apply appcNnil in H2. easy.
+       subst. apply appcNnil in H2. easy.
+       simpl.
        right. exact CIH.
 Qed.
 
 Inductive safe (R: tctx -> Prop): tctx -> Prop :=
-  | sasr   : forall p q s s' c, tctxRE (lsend p q (Some s)) c -> tctxRE (lrecv q p (Some s')) c ->
-                                tctxRE (lcomm p q) c -> safe R c
+  | sasr  : forall p q s s' c, tctxRE (lsend p q (Some s)) c -> tctxRE (lrecv q p (Some s')) c ->
+                               tctxRE (lcomm p q) c -> safe R c
   | saimpl:  forall p q c c', R c -> tctxRF (lcomm p q) c c' -> safe R c'.
 
 Definition safeC c := paco1 safe bot1 c.
 
-(* Definition alwaysN := @alwaysG nat.
-
-Definition alwaysCN F p := paco1 (alwaysN F) bot1 p.
-
-CoFixpoint Nnil := cocons 5 Nnil.
-
-Variant not_nil {A: Type}: (coseq A) -> Prop :=
-  | nnn : forall x xs, not_nil (cocons x xs).
-
-Lemma easyTry: alwaysCN not_nil Nnil.
-Proof. pcofix CIH.
-       pfold.
-       rewrite(coseq_eq Nnil). simpl.
-       constructor. constructor.
-       right. easy.
-Qed. *)
-
-(* CoInductive stream (A: Type) (B:Type): Type :=
-  | cnil : stream A B
-  | ccons: A -> B -> stream A B -> stream A B.
-
-Definition lblst_unf (A:Type) (B:Type) (pt: stream A B): stream A B :=
-  match pt with
-  | cnil => cnil A B
-  | ccons c l p => ccons A B c l p end.
-Arguments ccons {A} {B}.
-Arguments lblst_unf {A} {B}.
-
-Fixpoint onths {A} {B} (n: nat) (w: lbl_stream A B): option A :=
-  match (n, w) with
-    | (_, cnil)         => None
-    | (O, ccons x _ xs)   => Some x
-    | (S k, ccons x _ xs) => onths k xs
-  end.
-
-Notation path := (lbl_stream tctx label) (only parsing).
-
-Lemma lblst_unf_eq : forall (s:path), s=lblst_unf s.
-Proof.
-  intros.
-  destruct s.
-  auto. reflexivity.
+Lemma dom_app: forall c c', dom (appc c c') = dom c ++ dom c'.
+Proof. intro c.
+       induction c; intros.
+       - simpl. easy.
+       - simpl. destruct a. simpl. rewrite IHc. easy.
 Qed.
 
-
-Definition fair (pt: path): Prop :=
-  forall n p q c c' c'', 
-  (onths n pt) = Some c -> tctxRE c (lcomm p q) -> 
-  exists k, 
-    (onths k pt)     = Some c'  ->
-    (onths (S k) pt) = Some c'' ->
-    k >= n /\ tctxR c' (lcomm p q) c''.
-
-
-Inductive eventually (P: path -> Prop): Path -> Prop :=
-  
-
-
-
-Definition path_prop := path -> Prop.
-Definition context_prop := tctx -> Prop.
-Inductive eventually (F : path_prop) : path_prop:=
-    | ev_head : forall p: path, F p -> eventually F p
-    | ev_cons : forall (c:tctx) (p:path) (l:label), eventually F p -> eventually F (ccons c l p).
-
-(* imm_trans pth l p q <-> the immediate next transition in pth is between p and q*)
-Inductive imm_trans (p:part) (q:part) : path_prop :=
-  imm_trans_c : forall (pt:path) (c:tctx), imm_trans p q (ccons c (lcomm p q) pt). 
-
-Variant always_gen (F:path_prop) (alw_rel: path_prop): path_prop :=
-  | alw_gc: forall (c:tctx) (p:path) (l:label), F (ccons c l p) -> alw_rel p -> always_gen F alw_rel (ccons c l p).
-
-Hint Constructors always_gen.
-Definition always (F:path_prop) (p:path):= paco1 (always_gen F) bot1 p.
-Hint Unfold always.
-Lemma always_gen_mon: forall (F:path_prop), monotone1 (always_gen F).
-Proof.
-  pmonauto.
+Lemma _6_9: forall c c' l, tctxR c l c' -> dom c = dom c'.
+Proof. intros.
+       induction H; intros.
+       - simpl. easy.
+       - simpl. easy.
+       - simpl.
+         inversion H1. subst.
+         + inversion H0. 
+           ++ subst. simpl. easy.
+           ++ subst. simpl. simpl in *.
+              inversion IHtctxR2. subst. 
+              rewrite !dom_app. simpl.
+              inversion IHtctxR1.
+              rewrite !dom_app in H5.
+              inversion H5. rewrite H6. easy.
+         + subst. 
+           inversion H0.
+           ++ subst. simpl.
+              simpl in *.
+              inversion IHtctxR2. subst. easy. 
+           ++ subst. simpl in *.
+              rewrite !dom_app.
+              inversion IHtctxR1. subst.
+              inversion IHtctxR2. subst. simpl. subst.
+              rewrite !dom_app in H6 H7.
+              inversion H6. rewrite H8.
+              inversion H7. rewrite H9. easy.
+       - simpl. rewrite !dom_app. rewrite IHtctxR. easy.
 Qed.
-Hint Resolve always_gen_mon:paco.
-(*simple always example*)
-Variant not_nil : path_prop :=
-  | nnn : forall c l p, not_nil (ccons c l p).
 
-Definition always_not_nil := always not_nil.
-CoFixpoint inf_path := ccons tnil (lcomm "p" "q") inf_path. 
-Theorem inf_path_not_nil : always_not_nil inf_path.
-Proof.
-  pcofix CIH. pfold.
-  rewrite (lblst_unf_eq inf_path). simpl.
-  split.
-  {
-    constructor.
-  }
-  {
-    right. apply CIH.
-  }
-Qed.
-(*convert ctx prop to path prop i.e. for path p = c -l-> p', ctx_to_path_prop F p  iff F c*)
-(*a context prop holds for a path if it holds for the head of the path*)
-Variant ctx_to_path_prop (cp:context_prop): path_prop :=
-  | pt_head : forall c l p, cp c -> ctx_to_path_prop cp (ccons c l p).
-
-
-(*the transition enabled relation extended to paths*)
-Definition trans_enabled_path (l:label) : path_prop := ctx_to_path_prop (flip tctxRE l).
-(* fairness= always (enabled (p,q) -> eventually (transmitted p q))*)
-Definition fairness_inner (pt:path) : Prop := 
-  forall p q, trans_enabled_path (lcomm p q) pt -> eventually (imm_trans p q) pt.
-
-Definition fair_gfp := always (fairness_inner).
-
-Theorem inf_fair : fair_gfp inf_path.
-Proof.
-  pcofix CIH.
-  pfold.
-  rewrite (lblst_unf_eq inf_path). simpl.
-  split.
-  {
-    unfold fairness_inner.
-    intros.
-    apply ev_head.
-    (*for demonstration purposes i pretend tnil is actually the context obtained from projecting 
-    (mu t. p->q{l:t}) and i admit that the only comm transition possible from that context is (p,q)*)
-    assert(hax_0:p=("p":string)). admit.
-    assert(hax_1:q=("q":string)). admit.
-    subst.
-    apply imm_trans_c.
-  }
-  {
-    right.
-    apply CIH.
-  }
-Qed. *)
+Lemma _6_11a: forall p q (c c': ctx) s, tctxR (@und c) (lsend p q (Some s)) (@und c') -> 
+ exists xs n T, typeof (@und c) p = Some (ltt_send q xs) /\ onth n xs = Some (s, T). 
+Proof. intros.
+       inversion H.
+       - subst. simpl. rewrite eqb_refl.
+         exists xs. exists n. exists T. easy.
+       - subst. simpl.
+         inversion H3.
+Admitted.
+            
